aa1722e384a553fba6b355a98890e4889abf9d16
unknown <Matthias.Gondan@psy.ku.dk>
2023-11-06 08:43:11 +0100

    use xsb

--- xsb/estimate.bat
+++ xsb/estimate.bat
@@ -1,2 +1,3 @@
-xsb64 -e "consult('wuenic_work.pl')." -e "consult(data)." -e "estimate."
-"C:\Program Files\swipl\bin\swipl.exe" -g "(file_sha1('wuenic.out', SHA1), writeln(SHA1))" -g halt.
+call xsb64 -e "consult(wuenic_work), consult(bgd), estimate, halt."
+"C:\Progra~1\swipl\bin\swipl.exe" -g "(file_sha1('wuenic.out', SHA1), writeln(SHA1), halt)"
+echo 73af417f4276f63c8692a9404ce37bf1c9d56849
