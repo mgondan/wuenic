4a33e432630b6bed1f40cc497d9e95d5cf8cedac
unknown <Matthias.Gondan@psy.ku.dk>
2023-11-12 10:26:12 +0100

    improve output

--- xsb/estimate.bat
+++ xsb/estimate.bat
@@ -1,7 +1,4 @@
 call xsb64 --quietload --nobanner --noprompt -e "consult(wuenic_work), consult('%1'), estimate, halt."
-echo %1: New
-"C:\Progra~1\swipl\bin\swipl.exe" -g "(file_sha1('%1.new.txt', SHA1), writeln(SHA1), halt)"
-
 call xsb64 --quietload --nobanner --noprompt -e "consult(wuenic_ver_3), consult('%1'), estimate, halt." 2> old.txt
-echo Old
-"C:\Progra~1\swipl\bin\swipl.exe" -g "(file_sha1('%1.old.txt', SHA1), writeln(SHA1), halt)"
+echo %1
+"C:\Progra~1\swipl\bin\swipl.exe" -g "(file_sha1('%1.new.txt', New), file_sha1('%1.old.txt', Old), writeln(new-New), writeln(old-Old), halt)"
