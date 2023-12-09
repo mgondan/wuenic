a346cffe4a7c2fd80660dd6db697ee6f07b2d658
unknown <Matthias.Gondan@psy.ku.dk>
2023-12-09 19:16:35 +0100

    Done with Argentinia

--- xsb/estimate.bat
+++ xsb/estimate.bat
@@ -1,2 +1,4 @@
 swipl -g "consult(wuenic_ver_4), consult('%1'), estimate" -g halt
 swipl -g "(file_sha1('%1.v40.txt', V40), writeln(v40-V40))" -g halt
+
+rem "c:\Program Files\R\R-4.3.2\bin\x64\R.exe" --file=R\wuenic_ver_4.R --args arg
