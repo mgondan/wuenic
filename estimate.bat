77947eef308fb403407530bda7a99d8abe5950fa
unknown <Matthias.Gondan@psy.ku.dk>
2023-12-10 12:00:33 +0100

    Done with Austria

--- estimate.bat
+++ estimate.bat
@@ -1,5 +1,4 @@
 swipl -g "consult('xsb/wuenic_ver_4.pl'), consult('xsb/%1'), estimate" -g halt
-R --file=R\wuenic_ver_4.R --args %1
-swipl -g "(file_sha1('out/%1.v40.txt', V40), writeln(V40-v40))" -g halt
-swipl -g "(file_sha1('out/%1.R.txt', R), writeln(R-r))" -g halt
+R --file=R\wuenic_ver_4.R --quiet --no-echo --args %1
+swipl -g "(file_sha1('out/%1.v40.txt', V40), file_sha1('out/%1.R.txt', R), writeln(V40-v40), writeln(R-r))" -g halt
 
