ab8ecc7c3824353c6c9aef552cbba5433cc63063
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2024-02-17 20:14:35 +0100

    Cleanup and better explanations for surveys

--- R/w41_survey.R
+++ R/w41_survey.R
@@ -1,29 +1,31 @@
-# 5. Survey data
+# 5. Survey data. This is a bit more complicated, since we have multiple
+# surveys for the same year. Therefore, the survey results are stored in a 
+# three-dimensional array, Vaccine x Year x Survey Id.
 #
-# % Survey results passed for inclusion in the analysis include:
-# % card or history results for cohorts 12-23, 18-29, 15-26, 24-35 months of age
+# Analysis is in several steps
+# - select subset of surveys that apply for child diseases
+# - remove surveys with low information (e.g. sample size < 300)
+# - correct "recall bias" in surveys, e.g. when 3rd shot rate is implausibly
+#   high
+# - remove surveys when working group decisions say so
+# - average the available surveys for a given vaccine
+
+# Survey results passed for inclusion in the analysis include:
+# * card or history results
+# * cohorts 12-23, 18-29, 15-26, 24-35 months of age
+#
+# Prolog
 # survey_for_analysis(C, V, Y, ID, Description, Coverage) :-
 #     survey_results0(C, V, Y, ID, Description, Coverage),
 #     member(confirm:'card or history', Description),
 #     member(age:AgeCohort, Description),
 #     member(AgeCohort, ['12-23 m', '18-29 m', '15-26 m', '24-35 m']).
 
-cnf = Survey$Info.confirm == "card or history"
-age = Survey$Info.age %in% c("12-23 m", "18-29 m", "15-26 m", "24-35 m")
-index    = Survey[cnf & age, ]
-
-# MG, discuss: Only needed to find Ids for surveys that are ignored for multiple
-# reasons
-# Example: bcg,1999,afg2000231 ignored because of sample size as well as by a
-# wgd
-Dn = levels(as.factor(Survey$Id))
-Svy.CoH = array(NA_integer_, dim=c(length(Yn), length(Vn), length(Dn)), 
-                dimnames=list(Y=Yn, V=Vn, Id=Dn))
-Svy.CoH[cbind(index$Y, index$V, index$Id)] = index$Cov
-
-# % Reasons to exclude a survey include:
-# %    Sample size < 300,
-# %    The working group decides to exclude the survey.
+# Reasons to exclude a survey include:
+# * Sample size < 300,
+# * The working group decides to exclude the survey
+#
+# Prolog
 # survey_accepted(C, V, Y, ID, Coverage) :-
 #     survey_for_analysis(C, V, Y, ID, Desc, Cov0),
 #     (   decision(C, V, Y, acceptSurvey, _, ID, _)
@@ -34,28 +36,31 @@ Svy.CoH[cbind(index$Y, index$V, index$Id)] = index$Cov
 #     ),
 #     % Check if survey needs to be modified
 #     (   survey_modified(C, V, Y, ID, _, Modified)
-#     ->   Coverage = Modified
+#     ->  Coverage = Modified
 #     ;   Coverage = Cov0
 #     ).
 
-size     = Survey$Info.ss >= 300
-index    = Survey[cnf & age & size, ]
+Idn = levels(as.factor(Survey$Id))
+Svy.Ana = array(NA_integer_, dim=c(length(Yn), length(Vn), length(Idn)), 
+                dimnames=list(Y=Yn, V=Vn, Id=Idn))
 
-Dn = levels(as.factor(Survey$Id))
-Svy.Ana = array(NA_integer_, dim=c(length(Yn), length(Vn), length(Dn)), 
-                dimnames=list(Y=Yn, V=Vn, Id=Dn))
+conf = Survey$Info.confirm == "card or history"
+age  = Survey$Info.age %in% c("12-23 m", "18-29 m", "15-26 m", "24-35 m")
+size = Survey$Info.ss >= 300
+index = Survey[conf & age & size, ]
 Svy.Ana[cbind(index$Y, index$V, index$Id)] = index$Cov
 
-Svy.Title = array(NA_character_, dim=c(length(Yn), length(Vn), length(Dn)), 
-                  dimnames=list(Yn, Vn, Dn))
+index = Decisions[Decisions$Dec == "acceptSurvey", ]
+Svy.Ana[cbind(index$Y, index$V, index$Id)] = index$Cov
+
+index = Survey[conf & age & size, ]
+Svy.Title = array(NA_character_, dim=c(length(Yn), length(Vn), length(Idn)), 
+                  dimnames=list(Y=Yn, V=Vn, Id=Idn))
 Svy.Title[cbind(index$Y, index$V, index$Id)] = index$Info.title
 
-accept   = Decisions[Decisions$Dec == "acceptSurvey" & !is.na(Decisions$Id), ]
-Svy.Ana[cbind(accept$Y, accept$V, accept$Id)] = accept$Cov
-# MG, check: is a title needed if the survey is explicitly accepted?
-
-# % Recall bias is estimated by comparing the first and third dose of a vaccine
+# Recall bias is estimated by comparing the first and third dose of a vaccine
 #
+# Prolog
 # vaccine(dtp3, dtp1).
 # vaccine(pol3, pol1).
 # vaccine(hib3, hib1).
@@ -77,9 +82,9 @@ cnf = Survey$Info.confirm == "card"
 age = Survey$Info.age %in% c("12-23 m", "18-29 m", "15-26 m", "24-35 m")
 index = Survey[vac & cnf & age, ]
 
-Svy.C3 = array(NA_integer_, dim=c(length(Yn), length(V13), length(Dn)), 
-               dimnames=list(Y=Yn, V=V13, Id=Dn))
-Svy.C3[cbind(index$Y, index$V, index$Id)] = index$Cov
+Card3 = array(NA_integer_, dim=c(length(Yn), length(V13), length(Idn)), 
+              dimnames=list(Y=Yn, V=V13, Id=Idn))
+Card3[cbind(index$Y, index$V, index$Id)] = index$Cov
 
 #     % First dose, card or history
 #     vaccine(V, First),
@@ -93,9 +98,9 @@ cnf = Survey$Info.confirm == "card or history"
 age = Survey$Info.age %in% c("12-23 m", "18-29 m", "15-26 m", "24-35 m")
 index = Survey[vac & cnf & age, ]
 
-Svy.CH1 = array(NA_integer_, dim=c(length(Yn), length(names(V13)), length(Dn)), 
-                dimnames=list(Y=Yn, V=names(V13), Id=Dn))
-Svy.CH1[cbind(index$Y, index$V, index$Id)] = index$Cov
+CH1 = array(NA_integer_, dim=c(length(Yn), length(names(V13)), length(Idn)), 
+            dimnames=list(Y=Yn, V=names(V13), Id=Idn))
+CH1[cbind(index$Y, index$V, index$Id)] = index$Cov
 
 #     % First dose, card only
 #     survey_results0(C, First, Y, ID, DescriptionCard1Dose, C1Cov),
@@ -110,23 +115,23 @@ age = Survey$Info.age %in% c("12-23 m", "18-29 m", "15-26 m", "24-35 m")
 cv0 = Survey$Cov > 0
 index = Survey[vac & cnf & age & cv0, ]
 
-Svy.C1 = array(NA_integer_, dim=c(length(Yn), length(names(V13)), length(Dn)), 
-               dimnames=list(Y=Yn, V=names(V13), Id=Dn))
-Svy.C1[cbind(index$Y, index$V, index$Id)] = index$Cov
+Card1 = array(NA_integer_, dim=c(length(Yn), length(names(V13)), length(Idn)), 
+              dimnames=list(Y=Yn, V=names(V13), Id=Idn))
+Card1[cbind(index$Y, index$V, index$Id)] = index$Cov
 
 #     Adj is C3Cov / C1Cov,
 #     ThirdHistoryAdj is (CoH1Cov - C1Cov) * Adj,
 #     CovAdjusted is C3Cov + ThirdHistoryAdj,
 #     bound_0_100(CovAdjusted, Cov0),
 
-Adj = Svy.C3 / Svy.C1
-H3Adj = Adj * (Svy.CH1 - Svy.C1)
-H3Adj[] = pmin(99, pmax(0, tround(Svy.C3 + H3Adj)))
+Adj = Card3 / Card1
+H3Adj = Adj * (CH1 - Card1)
+H3Adj[] = pmin(99, pmax(0, tround(Card3 + H3Adj)))
 
 #     survey_for_analysis(C, V, Y, ID, Description, SurveyCoverage),
 #     Cov0 \= SurveyCoverage,
 
-# MG, discuss: Should we use some minimum distance instead of rounding?
+# Todo: Should we use some minimum distance instead of rounding?
 index = which(tround(Svy.Ana[, V13, , drop=FALSE]) != H3Adj, arr.ind=TRUE)
 
 #     SurveyCovRounded is round(SurveyCoverage),
@@ -142,32 +147,25 @@ index = which(tround(Svy.Ana[, V13, , drop=FALSE]) != H3Adj, arr.ind=TRUE)
 #         C3, ' percent. '], Expl),
 #         Coverage = Cov0.
 
-Svy.Info = array(NA_character_, dim=c(length(Yn), length(Vn), length(Dn)),
-                 dimnames=list(Y=Yn, V=Vn, Id=Dn))
+Svy.Info = array(NA_character_, dim=c(length(Yn), length(Vn), length(Idn)),
+                 dimnames=list(Y=Yn, V=Vn, Id=Idn))
 
 if(any(index))
 {
   Svy.Info[, V13, ][index] = sprintf(
     "%s card or history results of %.0f percent modifed for recall bias to %.0f percent based on 1st dose card or history coverage of %.0f percent, 1st dose card only coverage of %.0f percent and 3rd dose card only coverage of %.0f percent. ", 
-    Svy.Title[, V13, ][index], tround(Svy.Ana[, V13, ][index]),
-    H3Adj[index], tround(Svy.CH1[index]), tround(Svy.C1[index]), tround(Svy.C3[index]))
+    Svy.Title[, V13, ][index], tround(Svy.Ana[, V13, ][index]), H3Adj[index],
+    tround(CH1[index]), tround(Card1[index]), tround(Card3[index]))
   Svy.Ana[, V13, ][index] = H3Adj[index]
 }
 
 # Some surveys are ignored by the working group
-ignore = Decisions[Decisions$Dec == "ignoreSurvey" & !is.na(Decisions$Id), ]
-Svy.Acc = Svy.Ana
-Svy.Acc[cbind(ignore$Y, ignore$V, ignore$Id)] = NA
-Svy.Info[cbind(ignore$Y, ignore$V, ignore$Id)] = NA
+index = Decisions[Decisions$Dec == "ignoreSurvey", ]
+Svy.Acc = Svy.Ana # todo, collect explanations
+Svy.Acc[cbind(index$Y, index$V, index$Id)] = NA
+Svy.Info[cbind(index$Y, index$V, index$Id)] = NA
 
-## Some surveys are ignored by the working group (by year and vaccine, no Id)
-#ignore = Decisions[Decisions$Dec == "ignoreSurvey" & is.na(Decisions$Id), ]
-#if(nrow(ignore))
-#  for(i in 1:nrow(ignore))
-#  {
-#    Svy.Acc[ignore$Y[i], ignore$V[i], ] = NA
-#    Svy.Info[ignore$Y[i], ignore$V[i], ] = NA
-#  }
+# Todo: Collect explanations for Svy.Ana here, not later
 
 # % Survey information for given year. Multiple surveys are averaged.
 # survey(C, V, Y, Expl, Coverage) :-
