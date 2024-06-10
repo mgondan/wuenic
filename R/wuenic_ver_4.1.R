29390bb8673e46ff1f2eeb023c960d362410286a
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2024-06-10 23:31:48 +0200

    Nigeria

--- R/wuenic_ver_4.1.R
+++ R/wuenic_ver_4.1.R
@@ -3,6 +3,8 @@
 # irq: V41 is able to read the very large number in target/2
 # lao, nga, ton: avoid duplicate messages when survey is ignored by WG for
 #   multiple reasons
+# nga: avoid duplicate messages when reported data is excluded by WG for
+#   multiple reasons
 
 wuenic.estimate = function(ccode="syr", fname="countries/syr.pl", outname="wuenic.txt")
 {
