db7248cecb177896e6be47a328f2aee224b09b06
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2024-06-13 20:36:24 +0200

    add man page

--- R/01_mdb.R
+++ R/01_mdb.R
@@ -1,4 +1,18 @@
-mdb_get <- function(mdb="countries/wuenic2024.mdb", tab="ESTIMATE_REQUIRED")
+#' Interface to mdb-tools
+#' 
+#' @param mdb
+#' Path to MS Access database (extension mdb)
+#'
+#' @param tab
+#' Which table should be loaded
+#'
+#' @return
+#' Data frame with the requested table.
+#'
+#' @details
+#' Under Windows, install mdb-tools from Cygwin and put it into the PATH
+#'
+mdb_get <- function(mdb, tab)
 {
   f <- tempfile()
   system2(command="mdb-export", args=c(mdb, tab), stdout=f)
