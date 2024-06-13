83086dfcab596b5dcf18b2136d60d05a9dbe556c
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2024-06-13 20:40:30 +0200

    add some man pages

--- R/01_tround.R
+++ R/01_tround.R
@@ -1,4 +1,11 @@
-# Simulate Prolog's "outward" rounding
+#' Simulate Prolog's "outward" rounding
+#' 
+#' @param number
+#' a float
+#'
+#' @return
+#' rounded toward next even number
+#' 
 tround = function(number)
   sign(number) * trunc(abs(number) + 0.5 + sqrt(.Machine$double.eps))
 
