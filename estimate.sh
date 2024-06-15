7b985a846d79532fc1232f1712c02576df69a59c
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2024-06-15 16:43:30 +0200

    clean up

--- estimate.sh
+++
@@ -1,5 +0,0 @@
-BASE=`basename -s .pl $1`
-R --quiet --no-echo --file=R/wuenic_ver_4.R --args $BASE 2>warn4r.txt
-R --quiet --no-echo --file=R/wuenic_ver_4.1.R --args $BASE 2>warn41r.txt
-sha1sum out/$BASE.pl.R.txt
-sha1sum out/$BASE.R41.txt
--- pack.pl
+++
@@ -1,4 +0,0 @@
-name(wuenic).
-title('wuenic package').
-version('0.1').
-author('Matthias Gondan', 'matthias.gondan-rochon@uibk.ac.at').
